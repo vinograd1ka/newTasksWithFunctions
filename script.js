df01c3044a5dd0aeace6ed0426fd555a9d21ed95
vinograd1ka <kachanov220@mail.ua>
2021-01-26 22:34:03 +0200

    fix bags

--- script.js
+++ script.js
@@ -67,12 +67,12 @@
     //7 COMPLETE
 
     var substituteElement = function (array, change, whatToChange) {
-        return array.map(function(array,){
-            if (array === change){
-                array = whatToChange;
+        return array.map(function(item){
+            if (item === change){
+                item = whatToChange;
             }
-            return array;
-        }, 0);
+            return item;
+        });
     }
 
     var result7 = substituteElement([123, 1, 153, 51, 1], 1, true )
@@ -81,36 +81,27 @@
     //8 COMPLETE
 
     var arraysHasElement = function (array1, array2, val) {
-        var arrayBoolean1;
-        var arrayBoolean2;
+        var findEl1 = array1.find(function(item, ){
+                return item === val;
+        });
 
+        var findEl2 = array2.find(function(item, ){
+            return item === val;
+        });
 
-        return array1.reduce(function(result, item, i){
-            if(item === val){
-                arrayBoolean1 = true;
-            }
+        if(findEl1 === undefined)return false;
 
-            if(array2[i] === val){
-                arrayBoolean2 = true;
-            }
-
-            return arrayBoolean1 === true && arrayBoolean2 === true;
-
-        }, 0);
-
+        else return findEl2 !== undefined;
     }
-    var result8 = arraysHasElement([true,false, 5], [5, 1, true], 5);
+
+    var result8 = arraysHasElement([4, 2, 1], [1, 4, 3], 4);
     console.log(result8);
 
     //1 COMPLETE
 
     function filterFalsyValues(array) {
-        var pushVar = [];
-        return array.reduce(function(result, item){
-            if(item !== 0 && item !== null && item !== false && item !== undefined){
-                pushVar.push(item);
-            }
-            return pushVar;
+        return array.filter(function(item){
+            return  item !== 0 &&  item !== null &&  item !== false &&  item !== undefined;
         }, 0);
     }
 
@@ -120,46 +111,41 @@
     //2 COMPLETE
 
     function getMultipliedBigNumbers(array){
-        var check = [];
-        var num = [];
+        var itemMap = array.map(function(item){
+            return item*item;
+        });
 
-        return array.reduce(function(result, item, i, array) {
-            num.push(item * item);
-            if (num >= 1000) {
-                check.push(num);
-            }
-            return check;
-        }, 0);
+        return itemMap.filter(function(item){
+            return item >= 1000;
+        });
+
     }
-    var result12 = getMultipliedBigNumbers([32, 42, 55]);
+    var result12 = getMultipliedBigNumbers([32, 42, 55, 5, 10]);
     console.log(result12);
 
 
     //3 COMPLETE
-
     function getArrayValuesSum(array) {
         return array.reduce(function(result, item){
-            if(typeof item === 'number') {
-                result += item;
-                return result;
+            if(typeof item === 'number'){
+               result += item;
             }
-            else{
-                return item.length;
+            else if(typeof item === 'string'){
+                result += item.length;
             }
+            return result;
         }, 0);
     }
 
-    var result13 = getArrayValuesSum([1, 5, 152]);
+    var result13 = getArrayValuesSum([1, 'asdasdas',5 , 152, 'asfasf']);
     console.log(result13);
 
     //4 COMPLETE
 
     function reverseArray(array) {
-        var myArr = [];
         if(array.length >= 5){
             return array.reduce(function(result, item, i, array){
-                myArr = array;
-                return myArr.reverse();
+                return array.reverse();
             }, 0);
         }
         else{
@@ -203,21 +189,23 @@
     //3 COMPLETE
 
     function getArrayEqualElementCountHard(array1 ,array2) {
-        var arr1copy = array1.slice();
+        var count = 0;
+        array1.reduce(function(result, item, index) {
+            var findEl = result.findIndex(function(findItem){
+                return item === findItem;
+            });
 
-        return array2.reduce(function(result, item) {
-            var index = arr1copy.indexOf(item);
-            if (index !== -1) {
-                arr1copy.splice(index, 1);
-                result++;
+            if(findEl !== -1){
+                result.splice(findEl, 1);
+                count++;
             }
 
             return result;
-        }, 0);
+        }, array2);
+        return count;
     }
 
-
-    var result23 = getArrayEqualElementCountHard([1, 2, 1, 2, 3, 3], [3, 2, 1, 1]);
+    var result23 = getArrayEqualElementCountHard([1, 2, 3], [3, 2, 1]);
     console.log(result23);
 
     //4 COMPLETE
@@ -237,15 +225,12 @@
     //5 COMPLETE
 
     function getArrayElementByType(array, arrayTypeof) {
-        var check = [];
-        array.filter(function (item) {
-            if (typeof item === arrayTypeof)check.push(item)
+        return array.filter(function (item) {
+            return typeof item === arrayTypeof;
         });
-        return check;
     }
 
-    var result25 = getArrayElementByType([1, 'asd', 2, true, false, true, 3], 'string');
+    var result25 = getArrayElementByType([1, 'asd', 2, true, false, true, 3], 'boolean');
     console.log(result25);
-
 
 })();
